a3c5b5181f02ac6d903d32aa7f078d0b94a463c5
Mikael Jergefelt <mikael.jergefelt@ki.se>
2019-02-25 13:38:54 +0100

    stop animation by click

--- js/app.js
+++ js/app.js
@@ -19,8 +19,17 @@
     setHeight = function setHeight() {
       let tr808 = document.querySelector('ul');
       tr808.style.height = firstStep.offsetWidth * 2 + 'px';
+    },
+    animate = setInterval(runPattern, 100),
+    toggle = function toggle() {
+      if (animate) {
+        clearInterval(animate);
+        animate = null;
+      } else {
+        animate = setInterval(runPattern, 100);
+      }
     };
   setHeight();
-  setInterval(runPattern, 100);
   window.addEventListener('resize', setHeight, false);
+  document.querySelector('.tr-808').addEventListener('click', toggle, false);
 })();
