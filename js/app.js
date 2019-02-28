b1d1dd865118c6f693c68faf9c2a5b8ff9a2273f
Mikael Jergefelt <mikael.jergefelt@ki.se>
2019-02-28 10:47:17 +0100

    use requestanimationframe instead of setinterval

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
   let firstStep = document.querySelector('li'),
     currentStep = firstStep,
     motion = !matchMedia('(prefers-reduced-motion)').matches,
+    speed = 100,
     runPattern = function runPattern() {
       let jump = function jump(el) {
         el.classList.add('jump');
@@ -9,37 +10,38 @@
           el.classList.remove('jump');
         }, 125);
       };
-      if (currentStep && currentStep.nextElementSibling) {
-        requestAnimationFrame(function addNext() {
+      if (speed && motion) {
+        if (currentStep && currentStep.nextElementSibling) {
           currentStep.classList.remove('active');
           currentStep.nextElementSibling.classList.add('active');
           currentStep = currentStep.nextElementSibling;
-        });
-      } else {
-        requestAnimationFrame(function addFirst() {
+        } else {
           currentStep.classList.remove('active');
           firstStep.classList.add('active');
           currentStep = firstStep;
-        });
-      }
-      if (currentStep.classList.contains('selected')) {
-        jump(currentStep);
+        }
+        if (currentStep.classList.contains('selected')) {
+          jump(currentStep);
+        }
+        setTimeout(function again() {
+          requestAnimationFrame(runPattern);
+        }, speed);
       }
     },
     setHeight = function setHeight() {
       let tr808 = document.querySelector('ul');
       tr808.style.height = firstStep.offsetWidth * 1.75 + 'px';
     },
-    animate = motion ? setInterval(runPattern, 100) : null,
     toggle = function toggle() {
-      if (animate && motion) {
-        clearInterval(animate);
-        animate = null;
+      if (speed) {
+        speed = 0;
       } else {
-        animate = motion ? setInterval(runPattern, 100) : null;
+        speed = 100;
+        requestAnimationFrame(runPattern);
       }
     };
   setHeight();
+  requestAnimationFrame(runPattern);
   window.addEventListener('resize', setHeight, false);
   document.querySelector('.tr-808').addEventListener('click', toggle, false);
 })();
